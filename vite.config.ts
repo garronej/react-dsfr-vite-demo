3856d73b7a62f75c33fa136eb764e2168cfb78a2
garronej <joseph.garrone@data.gouv.fr>
2023-05-20 04:33:38 +0200

    Remove usless specific config

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    publicDir: "public",
-    server: {
-        fs: {
-          allow: ['../../..']
-        }
-      }
+    publicDir: "public"
 });
